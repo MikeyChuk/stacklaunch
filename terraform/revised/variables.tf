d4bf85b084a59b7672376b842e106e8fe73bf3b1
Michael Chukwunyere <m_chukwunyere@yahoo.co.uk>
2026-07-25 21:10:38 +0100

    rbac

--- terraform/revised/variables.tf
+++ terraform/revised/variables.tf
@@ -31,7 +31,7 @@ variable "public_subnet_cidrs" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster-dorkindor"
+  default     = "stacklaunch-eks"
 }
 
 variable "cluster_version" {
